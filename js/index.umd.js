d536787a471b47a9969218c7c91e02d03f0bbc71
Devkuamrjain <dj2328073@gmail.com>
2025-02-10 18:17:45 +0530

    Add files via upload

---
+++ js/index.umd.js
@@ -0,0 +1,34 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap index.umd.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import Alert from 'Quickstart.html/js/alert.js'
+import Button from 'Quickstart.html/js/button.js'
+import Carousel from 'Quickstart.html/js/carousel.js'
+import Collapse from 'Quickstart.html/js/collapse.js'
+import Dropdown from 'Quickstart.html/js/dropdown.js'
+import Modal from 'Quickstart.html/js/modal.js'
+import Offcanvas from 'Quickstart.html/js/offcanvas.js'
+import Popover from 'Quickstart.html/js/popover.js'
+import ScrollSpy from 'Quickstart.html/js/scrollspy.js'
+import Tab from 'Quickstart.html/js/tab.js'
+import Toast from 'Quickstart.html/js/toast.js'
+import Tooltip from 'Quickstart.html/js/tooltip.js'
+
+export default {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip
+}
